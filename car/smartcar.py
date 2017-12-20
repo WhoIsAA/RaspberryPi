9688d142f2cb86f1696c28972f1a1784c7bdc860
A-A <15918839960@139.com>
2017-12-20 17:24:46 +0800

    修改接口代码逻辑

--- car/smartcar.py
+++ car/smartcar.py
@@ -15,63 +15,74 @@ class SmartCar(object):
         self.init_infrared(config["infrared_obstacle"])
 
     def init_l298n(self, config):
-        self.l298n = L298N.L298N(config)
-        self.running = False
+        self._l298n = L298N.L298N(config)
 
     def init_sg90(self, config):
-        self.sg90_vt = SG90.SG90(config["vertical"])
-        self.sg90_hr = SG90.SG90(config["horizontal"])
+        self._sg90_vt = SG90.SG90(config["vertical"])
+        self._sg90_hr = SG90.SG90(config["horizontal"])
 
     def init_infrared(self, config):
-        self.infrared = Infrared.Infrared(config)
-        self.infrared.start()
+        self._infrared = Infrared.Infrared(config)
+        self._infrared.start()
+
+    def get_servo_angle_hr(self):
+        #获得水平舵机当前角度
+        return self._sg90_hr.get_current_angle()
+
+    def get_servo_angle_vt(self):
+        #获得垂直舵机当前角度
+        return self._sg90_vt.get_current_angle()
 
     def car_forward(self):
         #左右没有障碍物
-        if self.infrared.get_infrared_status() != "all":
-            self.l298n.forward()
+        if self._infrared.get_infrared_status() != "all":
+            self._l298n.forward()
 
     def car_backward(self):
-        self.l298n.backward()
+        self._l298n.backward()
 
     def car_front_turnleft(self):
         #左边没有障碍物
-        if self.infrared.get_infrared_status() != "left":
-            self.l298n.car_front_turnleft()
-
+        if self._infrared.get_infrared_status() != "left":
+            self._l298n.car_front_turnleft()
 
     def car_front_turnright(self):
         #右边没有障碍物
-        if self.infrared.get_infrared_status() != "right":
-            self.l298n.car_front_turnright()
+        if self._infrared.get_infrared_status() != "right":
+            self._l298n.car_front_turnright()
 
 
     def car_back_turnleft(self):
-        self.l298n.car_back_turnleft()
-
+        self._l298n.car_back_turnleft()
 
     def car_back_turnright(self):
-        self.l298n.car_back_turnright()
+        self._l298n.car_back_turnright()
 
     def car_stop(self):
-        self.l298n.stop()
+        self._l298n.stop()
+
+    def servo_hr_move_to(self, angle):
+        self._sg90_hr.move_to_new(angle)
+
+    def servo_vt_move_to(self, angle):
+        self._sg90_vt.move_to_new(angle)
 
     def servo_upward(self):
-        angle = self.sg90_vt.get_current_angle() - 10
-        self.sg90_vt.move_to(angle)
+        angle = self._sg90_vt.get_current_angle() - 10
+        self._sg90_vt.move_to(angle)
 
     def servo_downward(self):
-        angle = self.sg90_vt.get_current_angle() + 10
-        self.sg90_vt.move_to(angle)
+        angle = self._sg90_vt.get_current_angle() + 10
+        self._sg90_vt.move_to(angle)
 
     def servo_turnleft(self):
-        angle = self.sg90_hr.get_current_angle() + 10
-        self.sg90_hr.move_to(angle)
+        angle = self._sg90_hr.get_current_angle() + 10
+        self._sg90_hr.move_to(angle)
 
     def servo_turnright(self):
-        angle = self.sg90_hr.get_current_angle() - 10
-        self.sg90_hr.move_to(angle)
+        angle = self._sg90_hr.get_current_angle() - 10
+        self._sg90_hr.move_to(angle)
 
     def servo_reset(self):
-        self.sg90_hr.reset()
-        self.sg90_vt.reset()
+        self._sg90_hr.reset()
+        self._sg90_vt.reset()
--- handler/car.py
+++ handler/car.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 # coding=utf-8
-
+import atexit
+import json
+from RPi import GPIO
+from car.smartcar import SmartCar
 from handler.base import BaseHandler
+
+# 初始化GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+atexit.register(GPIO.cleanup)
+
+with open("./static/config.json") as file:
+    config = json.load(file)
+
+car = SmartCar(config)
 
 
 class ConnectTestHandler(BaseHandler):
 
     def _create_json(self):
         json_data = {}
-        json_data['hrAngle'] = 90
-        json_data['vtAngle'] = 90
+        json_data['hrAngle'] = car.get_servo_angle_hr()
+        json_data['vtAngle'] = car.get_servo_angle_vt()
         self.write(self.normal_json("connect successed", json_data))
 
     def get(self, *args, **kwargs):
@@ -23,9 +36,26 @@ class CarControlHandler(BaseHandler):
 
     def _create_json(self):
         action = self.get_argument('action')
+        if action == 'forward':
+            car.car_forward()
+        elif action == 'backward':
+            car.car_backward()
+        elif action == 'front_turnleft':
+            car.car_front_turnleft()
+        elif action == 'front_turnright':
+            car.car_front_turnright()
+        elif action == 'back_turnleft':
+            car.car_back_turnleft()
+        elif action == 'back_turnright':
+            car.car_back_turnright()
+        elif action == 'stop':
+            car.car_stop()
+        else:
+            return
+
         json_data = {}
-        json_data['hrAngle'] = 90
-        json_data['vtAngle'] = 90
+        json_data['hrAngle'] = car.get_servo_angle_hr()
+        json_data['vtAngle'] = car.get_servo_angle_vt()
         self.write(self.normal_json("car control, %s successed" % action, json_data))
 
     def get(self, *args, **kwargs):
@@ -40,10 +70,18 @@ class ServoControlHandler(BaseHandler):
     def _create_json(self):
         orientation = self.get_argument('orientation')
         angle = self.get_argument('angle')
+        if orientation == 0:
+            car.servo_hr_move_to(angle)
+        elif orientation == 1:
+            car.servo_vt_move_to(angle)
+        else:
+            return
+
         json_data = {}
-        json_data['hrAngle'] = 90
-        json_data['vtAngle'] = 90
-        self.write(self.normal_json("servo control, orientation:%s, angle:%s successed" % (orientation, angle), json_data))
+        json_data['hrAngle'] = car.get_servo_angle_hr()
+        json_data['vtAngle'] = car.get_servo_angle_vt()
+        self.write(
+            self.normal_json("servo control, orientation:%s, angle:%s successed" % (orientation, angle), json_data))
 
     def get(self, *args, **kwargs):
         self._create_json()
